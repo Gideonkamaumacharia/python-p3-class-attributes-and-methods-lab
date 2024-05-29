1918fff6beb856ef38701c45f151abf72090b001
Gideon Macharia <kamaugedion95@gmail.com>
2024-05-29 18:47:18 +0300

    complete lab

--- lib/song.py
+++ lib/song.py
@@ -1,21 +1,49 @@
 class Song:
     count = 0
     genres = []
-    def __init__(self,name,artist,genre):
+    artists = []
+    genre_count = {}
+    artist_count = {}  
+    
+
+    def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
-        self.add_song_to_count()
+        self.add_song_to_count()  
         self.add_to_genres(genre)
-    
+        self.add_to_artists(artist)  
+        self.update_genre_count(genre)  
+        self.update_artist_count(artist)  
+        
+
     @classmethod
-    def add_song_to_count(cls,increment = 1):
+    def add_song_to_count(cls, increment=1):
         cls.count += increment
+
     @classmethod
-    def add_to_genres(cls,genre):
-        cls.genres.append(genre)
+    def add_to_genres(cls, genre):
+        if genre not in cls.genres:  
+            cls.genres.append(genre)
+
+    @classmethod
+    def add_to_artists(cls, artist):  
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+
+    @classmethod
+    def update_genre_count(cls, genre):
+        if genre in cls.genre_count:
+            cls.genre_count[genre] += 1
+        else:
+            cls.genre_count[genre] = 1
+
+    @classmethod
+    def update_artist_count(cls, artist):
+        if artist in cls.artist_count:
+            cls.artist_count[artist] += 1
+        else:
+            cls.artist_count[artist] = 1
 
 
-ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
-
-print(ninety_nine_problems.name)
+ninety_nine_problems = Song("99 Problems", "Jay Z", "Rap")  
