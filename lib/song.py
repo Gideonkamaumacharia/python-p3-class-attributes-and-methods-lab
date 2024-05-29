f240251dd7bd77dd26527214108ae95f77e1f0e0
Gideon Macharia <kamaugedion95@gmail.com>
2024-05-29 17:48:47 +0300

    initial commit

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,21 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    def __init__(self,name,artist,genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        self.add_song_to_count()
+        self.add_to_genres(genre)
+    
+    @classmethod
+    def add_song_to_count(cls,increment = 1):
+        cls.count += increment
+    @classmethod
+    def add_to_genres(cls,genre):
+        cls.genres.append(genre)
+
+
+ninety_nine_problems = Song("99 Problems", "Jay-Z", "Rap")
+
+print(ninety_nine_problems.name)
